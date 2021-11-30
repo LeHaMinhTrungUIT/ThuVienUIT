65ed84041aa2d46e3809f49445eec5965c7f2511
LeHaMinhTrungUIT <18520390@gm.uit.edu.vn>
2021-11-30 16:15:15 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header() {
+
+return (
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+<div class="navbar-header">
+<a class="navbar-brand" href="#">Logo</a>
+</div>
+<ul class="nav navbar-nav">
+<li class="active"><a href="#">Trang chủ</a></li>
+<li><a href="#">Menu item 1</a></li>
+<li><a href="#">Menu item 1</a></li>
+<li><a href="#">Menu item 1</a></li>
+</ul>
+</div>
+</nav>
+);
+}
+
+export default Header;
