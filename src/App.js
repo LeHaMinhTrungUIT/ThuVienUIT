c667d816e2b77745359156942e77911b93dc1920
LeHaMinhTrungUIT <18520390@gm.uit.edu.vn>
2021-12-18 14:54:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header />        
+        <Header />     
       </header>
-
+      
       <Content />
-      <Sidebar />
-      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,21 @@ import React from 'react';
 
 function Header() {
 
-return (
-<nav class="navbar navbar-default">
-<div class="container-fluid">
-<div class="navbar-header">
-
-</div>
-<ul class="nav navbar-nav">
-<li class="active"><a href="#">Trang chủ</a></li>
-<li><a href="#">Quản lý thư viện</a></li>
-<li><a href="#">Hỗ trợ</a></li>
-<li><a href="#">Tải ứng dụng</a></li>
-<li><a href="#">Kết nối</a></li>
-
-</ul>
-</div>
-</nav>
-);
+    return (
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <ul id="nav">
+                    <li><a href="#">Quản lý thư viện</a></li>
+                    <li>|</li>
+                    <li><a href="#">Hỗ trợ</a></li>
+                    <li>|</li>
+                    <li><a href="#">Tải ứng dụng</a></li>
+                    <li>|</li>
+                    <li><a href="#">Kết nối</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
