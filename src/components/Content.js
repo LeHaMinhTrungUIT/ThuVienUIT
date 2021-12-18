ee318baee555c12fb0b1a1da5042fccd114aa918
LeHaMinhTrungUIT <18520390@gm.uit.edu.vn>
2021-12-18 14:54:03 +0700

    update

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,11 +2,30 @@ import React from 'react';
 
 function Content() {
 
-return (
-<article class="main-content">
-Nội dung nằm ở đây
-</article >
-);
+    return (
+        <div class="title">
+            <ul id="header2">
+                <li>
+                    <ul id="logoname">
+                        <li><img src="https://haitrieu.com/wp-content/uploads/2021/10/Logo-DH-Cong-Nghe-Thong-Tin-UIT.png"></img></li>
+                        <li>THƯ VIỆN TRƯỜNG ĐẠI HỌC CÔNG NGHỆ THÔNG TIN</li>
+                    </ul>
+                </li>
+                <li>
+                    <ul id="login">
+                        <li><a href="#">Đăng ký</a></li>
+                        <li>|</li>
+                        <li><a href='#'>Đăng nhập#</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+
+
+        </div>
+
+
+    );
 }
 
 export default Content;
