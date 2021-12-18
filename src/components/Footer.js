cae1dd0cab7d855d586278d76894360d00f7e20e
LeHaMinhTrungUIT <18520390@gm.uit.edu.vn>
2021-12-18 17:06:50 +0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,36 @@ import React from 'react';
 
 function Footer() {
 
-return (
-<footer class="footer">
-<h1>Tải app thư viện UIT ngay.</h1>
-</footer>
-);
+    return (
+        <footer class="footer">
+            <ul id="footercontent">
+                <li>
+                    <ul class="footercontent">
+                        <li class="footertitle"> Tải App Thư viện UIT ngay</li>
+                        <li>Tiện lợi, dễ sử dụng, mọi lúc mọi nơi.</li>
+                        <li>Đem lại những trải nghiệm tuyệt vời.</li>
+                    </ul>
+                </li>
+                <li>
+                    <ul class="footercontent">
+                        <li class="footertitle">Giới thiệu</li>
+                        <li>Nhân sự</li>
+                        <li>Nội quy thư viện</li>
+                        <li>Tài nguyên thư viện</li>
+                        <li>Chức năng, nhiệm vụ</li>
+                    </ul>
+                </li>
+                <li>
+                    <ul class="footercontent">
+                        <li class="footertitle">Liên hệ</li>
+                        <li>(08) 372 51993-128</li>
+                        <li>Góp ý</li>
+                    </ul>
+                </li>
+            </ul>
+            <h3 id="lisence"> © 2021 Trường đại học Công nghệ Thông tin | ĐHQG TP.HCM</h3>
+        </footer>
+    );
 }
 
 export default Footer;
